63a9f870d9e1904d881b115455179ffb2abfed7c
coolgray06 <devaughn918@gmail.com>
2023-05-30 15:51:56 -0700

    making it through

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -4,16 +4,19 @@
 
 /**
  * main - Entry point
+ *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 	char low;
+
 	for (low = 'a'; low <= 'z'; low++)
-	putchar(low);
-	for (low) = 'A'; low <= 'Z'; low++;)
-	putchar(low)
-	putchar ('\n')
+		putchar(low);
+	for (low = 'A'; low <= 'Z'; low++)
+		putchar(low);
+	putchar('\n');
+
 	return (0);
 }
