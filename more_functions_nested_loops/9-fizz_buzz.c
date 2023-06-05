14bf02efd38366c15d906c5da2dfde22034b6853
coolgray06 <devaughn918@gmail.com>
2023-06-05 16:50:47 -0700

    see the light

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints a Fizz Buzz program 
+ * main - Prints a Fizz Buzz program
  * Return: Always 0 (Success)
  */
 
@@ -10,21 +10,18 @@ int main(void)
 {
 	int x = 1;
 
-	for (; x<= 100; x++)
+	for (; x <= 100; x++)
 	{
-	if ((x % 3 == 0) && (x % 5 == 0))
-	{
-	printf("FizzBuzz");
-	}
-	else if (x % 5 == 0)
-	{
-	printf("Buzz");
-	}
-	else
-	{
-	printf("%d", x);
+		if ((x % 3 == 0) && (x % 5 == 0))
+			printf("FizzBuzz");
+		else if (x % 3 == 0)
+			printf("Fizz ");
+		else if (x % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d", x);
 	}
 	printf("Buzz\n");
-	}
+
 	return (0);
 }
