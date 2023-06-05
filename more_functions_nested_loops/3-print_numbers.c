be265a6f761d6270f2f7fb6a04ef069cf7cc9f21
coolgray06 <devaughn918@gmail.com>
2023-06-05 09:50:16 -0700

    getting close

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -2,17 +2,16 @@
 
 /**
  * print_numbers - Print the numbers 0 up to 9
+ * followed by a new line.
  * Return: The numbers 0 up to 9
  */
-
 void print_numbers(void)
-
 {
-	int c;
+	int c = '0';
 
 	for (c = 0; c <= 9; c++)
 	{
-		_putchar(c + '0');
+	_putchar(c + '0');
 	}
 	_putchar('\n');
 }
