116d33d11fad6c61dbb3c19c33fc806ecfb70ff7
coolgray06 <devaughn918@gmail.com>
2023-06-05 09:27:32 -0700

    getting close

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,8 +1,26 @@
 #ifndef HEADER_H
 #define HEADER_H
 
+int _putchar(char c);
+
+int _isupper(int c);
+
+int _isdigit(int c);
+
+int mul(int a, int b);
+
 void print_numbers(void);
 
-int _putchar(int char)
+void print_most_numbers(void);
+
+void more_numbers(void);
+
+void print_line(int n);
+
+void print_diagonal(int n);
+
+void print_square(int size);
+
+void print_triangle(int size);
 
 #endif
