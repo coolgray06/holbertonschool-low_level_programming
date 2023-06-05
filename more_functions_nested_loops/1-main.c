5cf6af6ee81fc1596f19bfc714dc18ef5b73b42d
coolgray06 <devaughn918@gmail.com>
2023-06-05 07:19:15 -0700

    keep going

--- more_functions_nested_loops/1-main.c
+++ more_functions_nested_loops/1-main.c
@@ -1,16 +1,18 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
+ * main - check the code
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	char c;
+    char c;
 
-	for (c = '0'; c <= '9'; ++c)
-		printf("%c: %d\n", c, (_isdigit(c));
-	return (0);
+    c = '0';
+    printf("%c: %d\n", c, _isdigit(c));
+    c = 'a';
+    printf("%c: %d\n", c, _isdigit(c));
+    return (0);
 }
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,6 +1,6 @@
 #ifndef HEADER_H
 #define HEADER_H
 
-int _isupper(int c);
+int _isdigit(int c);
 
 #endif
