090b27838877d549e4bf8989fee56b100c4013bd
coolgray06 <devaughn918@gmail.com>
2023-06-12 20:14:25 -0700

    it'll be fun they said

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_chessboard - Prints the chessboard
+ * @a: array
+ * Return: Always 0
+ */
+
+void print_chessboard(char (*a)[8])
+
+{
+	int i = 0;
+	int n = 0;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (n = 0; n < 8; n++)
+		{
+			_putchar(a[i][n]);
+		}
+		_putchar('\n');
+	}
+}
