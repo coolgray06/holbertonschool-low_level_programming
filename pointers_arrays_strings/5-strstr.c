c2a728c7bfb677cede0031d8571035ccef9afd5b
coolgray06 <devaughn918@gmail.com>
2023-06-12 19:54:04 -0700

    it'll be fun they said

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strstr - Locates a substring
+ * @haystack: String
+ * @needle: Substring
+ * Return: Always 0
+ */
+
+char *_strstr(char *haystack, char *needle)
+
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *s1 = haystack;
+		char *s2 = needle;
+
+		while (*s1 == *s2 && *s2 != '\0')
+		{
+			*s1++;
+			*s2++;
+		}
+		if (*s2 == '\0')
+			return (haystack);
+	}
+	return ('\0')
+}
