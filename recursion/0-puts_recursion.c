e34d66f991bf0753934a561d9ebb2c67229f630a
coolgray06 <devaughn918@gmail.com>
2023-06-17 17:19:02 -0700

    keep  on, keeping on

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Prints a string,followed by a new line.
+ * @s: String
+ *
+ * Return: Void
+ */
+
+void _puts_recursion(char *s)
+
+{
+	if (*s)
+	{
+		_putchar(*s)
+		_puts_recursion(s + 1)
+	}
+	else
+	{
+	_putchar('\n');
+	}
+}
